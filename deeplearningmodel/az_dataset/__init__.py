dac23e7e990ca872914b8c21f675ec2e20e4e13d
Shivam Gupta <shivamg.cr7@gmail.com>
2020-09-07 20:49:27 +0530

    [Shivam] Using helpers.py file to load A-Z handwritten digit dataset and MNIST dataset.

---
+++ deeplearningmodel/az_dataset/__init__.py
@@ -0,0 +1,3 @@
+# import the necessary packages
+from .helpers import load_mnist_dataset
+from .helpers import load_az_dataset
---
+++ deeplearningmodel/az_dataset/helpers.py
@@ -0,0 +1,42 @@
+# import the necessary packages
+from tensorflow.keras.datasets import mnist
+import numpy as np
+
+def load_az_dataset(datasetPath):
+	# initialize the list of data and labels
+	data = []
+	labels = []
+
+	# loop over the rows of the A-Z handwritten digit dataset
+	for row in open(datasetPath):
+		# parse the label and image from the row
+		row = row.split(",")
+		label = int(row[0])
+		image = np.array([int(x) for x in row[1:]], dtype="uint8")
+
+		# images are represented as single channel (grayscale) images
+		# that are 28x28=784 pixels -- we need to take this flattened
+		# 784-d list of numbers and repshape them into a 28x28 matrix
+		image = image.reshape((28, 28))
+
+		# update the list of data and labels
+		data.append(image)
+		labels.append(label)
+
+	# convert the data and labels to NumPy arrays
+	data = np.array(data, dtype="float32")
+	labels = np.array(labels, dtype="int")
+
+	# return a 2-tuple of the A-Z data and labels
+	return (data, labels)
+
+def load_mnist_dataset():
+	# load the MNIST dataset and stack the training data and testing
+	# data together (we'll create our own training and testing splits
+	# later in the project)
+	((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
+	data = np.vstack([trainData, testData])
+	labels = np.hstack([trainLabels, testLabels])
+
+	# return a 2-tuple of the MNIST data and labels
+	return (data, labels)
